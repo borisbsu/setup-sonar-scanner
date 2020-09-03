eddff20e8badba15f2480a3c33f9a494a3b97751
Barys Yakavita <borisbsu@gmail.com>
2020-09-03 21:49:29 +0300

    Commit

--- index.js
+++ index.js
@@ -2,35 +2,34 @@ const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 const path = require('path');
 
-const TOOL_NAME = 'sonar-scanner'
+const TOOL_NAME = 'sonar-scanner';
 
 const getOs = () => {
   switch(process.platform) {
     case 'win32':
-      return 'windows'
+      return 'windows';
     case 'darwin':
-      return 'macosx'
+      return 'macosx';
     default:
       return 'linux'
   }
-}
+};
 
 const makeUrl = (version) => {
   return `https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${version}-${getOs()}.zip`
-}
+};
 
 const makeDirName = (version) => {
   return `${TOOL_NAME}-${version}-${getOs()}`
-}
+};
 
 const download = async (version) => {
   const url = makeUrl(version);
   const toolPath = await tc.downloadTool(url);
   const toolExtractedPath = await tc.extractZip(toolPath);
   const cachedPath = await tc.cacheDir(toolExtractedPath, TOOL_NAME, version);
-  const bin = path.join(cachedPath, makeDirName(version), 'bin');
-  return bin;
-}
+  return path.join(cachedPath, makeDirName(version), 'bin');
+};
 
 const findInCache = (version) => {
   const dir = tc.find(TOOL_NAME, version);
@@ -39,7 +38,7 @@ const findInCache = (version) => {
   } else {
     return null;
   }
-}
+};
 
 const run = async () => {
   try {
@@ -53,6 +52,6 @@ const run = async () => {
     // setFailed logs the message and sets a failing exit code
     core.setFailed(`Action failed with error ${err}`);
   }
-}
+};
 
 run();
